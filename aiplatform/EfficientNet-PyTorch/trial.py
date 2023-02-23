69e45cac206003180a6f63b0b35a08a9908a7492
tuannc <tuanhazard104@gmail.com>
2023-02-24 02:15:05 +0700

    fix efficientnet

---
+++ aiplatform/EfficientNet-PyTorch/trial.py
@@ -0,0 +1,17 @@
+from efficientnet_pytorch import EfficientNet
+import torch
+model = EfficientNet.from_pretrained('efficientnet-b0')
+img = torch.rand(1,3,224,224)
+# ... image preprocessing as in the classification example ...
+print(img.size()) # torch.Size([1, 3, 224, 224])
+
+features = model.extract_features(img)
+print(features.size()) # torch.Size([1, 1280, 7, 7])
+
+# endpoints = model.extract_endpoints(img)
+# print(endpoints['reduction_1'].shape)  # torch.Size([1, 16, 112, 112])
+# print(endpoints['reduction_2'].shape)  # torch.Size([1, 24, 56, 56])
+# print(endpoints['reduction_3'].shape)  # torch.Size([1, 40, 28, 28])
+# print(endpoints['reduction_4'].shape)  # torch.Size([1, 112, 14, 14])
+# print(endpoints['reduction_5'].shape)  # torch.Size([1, 320, 7, 7])
+# print(endpoints['reduction_6'][0][0])  # torch.Size([1, 1280, 7, 7])
